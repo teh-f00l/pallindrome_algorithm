90ad66d1893f7e5a0683c6e50e579e775afe3d53
teh-f00l <vihavira@protonmail.com>
2018-08-06 09:54:44 -0400

    to make sure I can do this the old fashoned way

---
+++ pallindrome.js
@@ -0,0 +1,17 @@
+// There are easier ways but I needed to make sure I really get JS syntax. Also lowercase only letter strings assumed
+function pallindrome(str) {
+    
+    for (i=0;i<str.length;i++){
+        if (str[i] == str[str.length-i-1]){
+			console.log(str[i]);
+            console.log(str[str.length-i-1]); // print diagnostics
+            return false;
+        }
+   
+    }
+	return true;
+
+}
+
+
+console.log(pallindrome("haolah"));
